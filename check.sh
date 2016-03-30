aa54e5909db4fb48af24583a82b4a1c8d2465a99
Luke Shirnia <lukeshirnia@btinternet.com>
2016-03-30 11:54:02 +0100

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,6 @@
+read -p "What site would you like to check? " sitecheck
+{  sleep 2; \
+( strace -o /tmp/output -f -r -s4096 -p `pidof telnet` &  ) ; \
+printf "GET / HTTP/1.1\n"; \
+printf "Host: $sitecheck\n"; echo ""; \
+sleep 2; } |  telnet 127.0.0.1 80
