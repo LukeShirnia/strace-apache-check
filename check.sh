171d4cc9052f2b8a67c4a2a6e9c539120f5fa1d1
Luke Shirnia <lukeshirnia@btinternet.com>
2016-03-31 14:40:30 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -5,20 +5,28 @@ validation=0
 function organise {
 test=$(awk '{print $2 }' /tmp/$default | sort -rn | head -5)
 grep "$test" /tmp/$default > /tmp/stracetop
-echo "top offenders: $test"
+#echo "top offenders: $test"
+}
+function sitelist {
+
+sites=$(/usr/sbin/httpd -S 2>&1 | grep "port 80 namevhost" | awk '{print $4}')
+
 }
 
+sitelist
 while [ "$validation" -le 0 ]; do
-        read -p "What site would you like to check? " sitecheck
+        printf "Port 80 sites: \n\n"
+        printf "$sites\n\n"
+        read -p "What local site would you like to check? " sitecheck
         validation=$( /usr/sbin/httpd -S 2>&1 | grep "namevhost $sitecheck" | wc -l )
-        echo $validation
+        echo $validationo
 done
 
 while [[ ! ("$filenameyn" =~ (y|ye|yes)$ ) ]]; do
-read -p "Specify strace file name? (y/N) " filenameyn
+read -p "Specify strace file name? (Default: /tmp/output) (y/N) " filenameyn
   case $filenameyn in
     y|ye|yes )
-     read -p "What filename would you like? " default
+     read -p "What filename would you like? /tmp/ " default
     ;;
     n|N|no )
       break
@@ -42,4 +50,4 @@ sleep 2 ;\
 ( strace -o /tmp/$default -f -r -s4096 -p `pidof telnet` &  ) ; \
 sleep 2 ;\
 printf "Host: $sitecheck\n"; echo ""; \
-} |  telnet 127.0.0.1 80 | grep 'HTTP/1.1\|Date:\|Server:\|Last-Modified:\|Content-Type:' || organise
+} |  telnet 127.0.0.1 80 | grep 'HTTP/1.1\|Date:\|Server:\|Last-Modified:\|Content-Type:'  || organise
