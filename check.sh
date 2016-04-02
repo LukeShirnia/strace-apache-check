a646c13a299534feb210993d24037330c29a41ee
Luke Shirnia <lukeshirnia@btinternet.com>
2016-04-02 12:55:09 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,20 +2,30 @@
 default="output"
 validation=0
 
-function organise {
-test=$(awk '{print $2 }' /tmp/$default | sort -rn | head -5)
-grep "$test" /tmp/$default > /tmp/stracetop
-}
-function sitelist { 
+function sitelist {
 sites=$(/usr/sbin/httpd -S 2>&1 | grep "port 80 namevhost" | awk '{print $4}')
 }
 
 function sitelistubuntu {
 sites=$(/usr/sbin/apache2 -S 2>&1 | grep "port 80 namevhost" | awk '{print $4}')
 }
-
-check_distro() { #checking the OS distribution
-if [ ! -f /etc/redhat-release ]; then #if the OS is NOT red hat or CentOS 
+function telnetcommands {
+echo "GET / HTTP/1.1"
+sleep 2
+( strace -o /tmp/$default -f -r -s4096 -p `pidof telnet` &  )
+#( strace -o /tmp/$default -f -r -s4096 -e trace=sendto,connect,open,write,read -p `pidof telnet` &  )
+sleep 1
+echo "Host: $sitecheck"
+echo ""
+#sleep 2
+}
+function organise {
+sort -k2rn /tmp/$default | head > /tmp/stracesort
+echo "Top 10 slowest system calls:"
+cat /tmp/stracesort
+}
+check_distro() {
+if [ ! -f /etc/redhat-release ]; then
         Distro=$(cat /etc/issue | head -1 | cut -d' ' -f1)
         if  [ $Distro == "Ubuntu"] || [ $Distro == "Debian" ]; then
                 case $Distro in
@@ -27,10 +37,10 @@ if [ ! -f /etc/redhat-release ]; then #if the OS is NOT red hat or CentOS
                 ;;
                 esac
         fi
-elif [ `cat /etc/redhat-release | grep -i "hat" | wc -l` -gt 0 ]; then #if the OS is red hat then get version number
+elif [ `cat /etc/redhat-release | grep -i "hat" | wc -l` -gt 0 ]; then
                 Distro="Red Hat"
                 Version=$(cat /etc/redhat-release | grep -Eo '[0-9]{1,4}' | head -1)
-elif  [ `cat /etc/redhat-release | grep -i "centos" | wc -l` -gt 0 ]; then #if the OS is CentOS then get vesion number
+elif  [ `cat /etc/redhat-release | grep -i "centos" | wc -l` -gt 0 ]; then
                 Distro="CentOS"
                 Version=$(cat /etc/redhat-release | grep -Eo '[0-9]{1,4}' | head -1)
 fi
@@ -42,17 +52,32 @@ while [ "$validation" -le 0 ]; do
         printf "Port 80 sites: \n\n"
         printf "$sites\n\n"
         read -p "What local site would you like to check? " sitecheck
-                if [ $Distro == "CentOS" ] || [ $Distro == "Red Hat" ]; then
-                        validation=$( /usr/sbin/httpd -S 2>&1 | grep "namevhost $sitecheck" | wc -l )
-                elif [ $Distro == "Ubuntu" ] || [ $Distro = 'Debian' ]; then
-                        validation=$( /usr/sbin/apache2 -S 2>&1 | grep "namevhost $sitecheck" | wc -l )
-                fi
+if [ $Distro == "CentOS" ] || [ $Distro == "Red Hat" ]; then
+        validation=$( /usr/sbin/httpd -S 2>&1 | grep "namevhost $sitecheck" | wc -l )
+elif [ $Distro == "Ubuntu" ] || [ $Distro = 'Debian' ]; then
+        validation=$( /usr/sbin/apache2 -S 2>&1 | grep "namevhost $sitecheck" | wc -l )
+fi
         echo $validation
 done
 }
 
+function outputfilename {
+read -p "Specify strace file name? (Default: /tmp/output) (y/N) " filenameyn
+  case $filenameyn in
+    y|ye|yes )
+     read -p "What filename would you like? /tmp/ " default
+    ;;
+    n|N|no )
+      break
+    ;;
+    * )
+      printf "Please enter a valid option"
+      printf "\n----------------------------------------------\n"
 
-        organise
+    ;;
+    esac
+}
+
         check_distro
         if [ $Distro == "CentOS" ] && [ $Version -lt 7 ] || [ $Distro == "Red Hat" ] && [ $Version -lt 7 ]; then
                 sitelist
@@ -68,29 +93,13 @@ done
                 whichsite
         fi
 
-while [[ ! ("$filenameyn" =~ (y|ye|yes)$ ) ]]; do
-read -p "Specify strace file name? (Default: /tmp/output) (y/N) " filenameyn
-  case $filenameyn in
-    y|ye|yes )
-     read -p "What filename would you like? /tmp/ " default
-    ;;
-    n|N|no )
-      break
-    ;;
-    * )
-      printf "Please enter a valid option"
-      printf "\n----------------------------------------------\n"
+        while [[ ! ("$filenameyn" =~ (y|ye|yes)$ ) ]]; do
+                outputfilename
+        done
 
-    ;;
-    esac
-done
-{ printf "GET / HTTP/1.1\n"; \
-sleep 2 ;\
-( strace -o /tmp/$default -f -r -s4096 -p `pidof telnet` &  ) ; \
-sleep 2 ;\
-printf "Host: $sitecheck\n"; echo ""; \
-} |  telnet 127.0.0.1 80 | grep 'HTTP/1.1\|Date:\|Server:\|Last-Modified:\|Content-Type:'  || organise
 
+        telnetcommands | telnet 127.0.0.1 80 | grep 'HTTP/1.1\|Date:\|Server:\|Last-Modified:\|Content-Type:'
+        organise
 
 #add php specific section:
 # function docroot {
@@ -102,8 +111,6 @@ printf "Host: $sitecheck\n"; echo ""; \
 
 ########Actual Command########
 #HTTP_HOST=$sitecheck REQUEST_URI=/ strace -o /tmp/phptrace -r -e trace=sendto,connect,open,write,read php "$docroot""index.php" > /dev/null
-
-
 
 #else
 #dont do it
